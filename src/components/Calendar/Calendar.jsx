e2343819f39393e091d76c552d5f7543af2cd190
Joey Pettit <pettitbass@gmail.com>
2022-10-18 23:52:01 -0500

    calendar working, about to fix props issue

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -6,14 +6,13 @@ import {useState, useEffect} from 'react';
 
 function Calendar(){
     const todaysDate = DateTime.now();
+
     console.log('in Calendar, todays date is:', todaysDate.toFormat('yyyy'));
 
     // These pieces of state track which month and year we are currently viewing
     // default states are set to the month/year of today's date (local time);
     const [monthViewed, setMonthViewed] = useState(Number(todaysDate.toFormat('L')));
     const [yearViewed, setYearViewed] = useState(Number(todaysDate.toFormat('yyyy')));
-
-
 
 
 
