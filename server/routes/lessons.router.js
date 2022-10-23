9e6fe8d6fa6d9631a606948c1ea04371468d2de6
Joey Pettit <pettitbass@gmail.com>
2022-10-23 15:54:47 -0500

    instructor lesson select query now returns array of registered students

--- server/routes/lessons.router.js
+++ server/routes/lessons.router.js
@@ -16,44 +16,50 @@ router.get('/instructor', rejectUnauthenticated, (req, res) => {
         console.log('Error Selecting lessons', error);
         res.sendStatus(500);
     });
-})
+});
 
 // this route selects the available lessons of an instructor (by a different user (student))
 // returns an object with instructor info and array of lesson objects (from lessons table)
 router.get('/instructor/:instructorid', rejectUnauthenticated, (req, res) => {
     let instructor_id = req.params.instructorid;
+    console.log('in instructor route', instructor_id);
 
-    let queryText = `SELECT "last_name", "first_name", "instructor_id", "lessons"."id" AS "lesson_id", 
-                        "start_time", "end_time" FROM "lessons"
+    let queryText = `SELECT "lessons"."id" AS "lesson_id", "last_name", "first_name", "instructor_id", 
+                        "start_time", "end_time", array_agg("students_lessons"."student_id") AS "registered_students_ids" FROM "lessons"
                         JOIN "user" ON "user"."id" = "lessons"."instructor_id"
-                        WHERE "instructor_id" = $1 AND "is_available"=true;`
+                        LEFT JOIN "students_lessons" ON "students_lessons"."lesson_id"="lessons"."id"
+                        WHERE "instructor_id" = $1
+                        GROUP BY "lessons"."id","last_name", "first_name", "instructor_id", 
+                        "start_time", "end_time";`;
     pool
     .query(queryText,[instructor_id])
     .then((response)=> {
         console.log('in Select available lessons route', response.rows);
-        res.send(response.rows)
+        res.send(response.rows);
     })
     .catch((error) => {
         console.log('Error Selecting lessons', error);
         res.sendStatus(500);
     });
-})
+});
 
-// this route will reserve the lesson of included ID for the logged in user
-router.put('/reserve-lesson/:lessonid', rejectUnauthenticated, (req, res) => {
+// this route will reserve the lesson of included ID for the logged in user(student)
+router.post('/reserve-lesson/:lessonid', rejectUnauthenticated, (req, res) => {
     let lessonId = req.params.lessonid;
+    let studentId = req.user.id;
+    console.log('in reserve lesson route, student id is', studentId,'lesson id is', lessonId);
     
-    let queryText = ``;
+    let queryText = `INSERT INTO "students_lessons" ("student_id", "lesson_id")
+                                            VALUES  ($1, $2);`;            
+    pool
+    .query(queryText, [studentId, lessonId])
+    .then((response)=> res.sendStatus(201))
+    .catch((error)=>console.log('Error with reserving lesson', error));
+});
 
 
 
-})
-
-
-
-/*
- * POST route template
- */
+// this allows instructor to add an available lesson time.
 router.post('/add-lesson', rejectUnauthenticated, (req, res) => {
     let {start_time, end_time} = req.body;
     let instructor_id = req.user.id;
@@ -61,24 +67,17 @@ router.post('/add-lesson', rejectUnauthenticated, (req, res) => {
     console.log('IN ADD LESSON ROUTE, id=', req.user.id, 'start=', start_time, 'end=', end_time);
     
     let queryText = `INSERT INTO "lessons" 	("start_time", "end_time", "instructor_id")
-                    VALUES					($1, $2, $3) RETURNING "lessons"."id";`
+                        VALUES					($1, $2, $3);`
     pool
     .query(queryText, [start_time, end_time, instructor_id])
-    .then((response) => {
-        console.log('in the response,', response.rows[0].id);
-        let queryText2 = `INSERT INTO "students_lessons" ("lesson_id") VALUES ($1);`;
-
-        pool
-        .query(queryText2, [response.rows[0].id])
-        .then((response2) => res.sendStatus(201))
-        .catch(error => console.log('Error with inserting into join table', error));
-        })
+    .then((response) => res.sendStatus(201))
     .catch((error)=> {
         console.log('Errow with posting new lesson', error)
         res.sendStatus(500);
     });
 });
 
+// This router allows an instructor to delete an available lesson time.
 router.delete('/delete-lesson/:lessonid', rejectUnauthenticated, (req, res) => {
     let lessonId = req.params.lessonid;
     let instructor_id = req.user.id;
@@ -92,9 +91,10 @@ router.delete('/delete-lesson/:lessonid', rejectUnauthenticated, (req, res) => {
         pool
         .query(queryText2, [instructor_id, lessonId])
         .then((response) => res.sendStatus(204))
+        .catch((error) => console.log('Error with second delete query'));
     })  
     .catch((error)=> {
-        console.log('Errow with deleting lesson', error)
+        console.log('Error with deleting lesson', error)
         res.sendStatus(500);
     });
 });
