547bdf8149bf1b97537b150c3b3fb09d49423f2b
Joey Pettit <pettitbass@gmail.com>
2022-10-25 20:19:56 -0500

    removed modal from homepage

--- src/components/PortalInstructor/InstructorCalendar/InstructorCalendar.jsx
+++ src/components/PortalInstructor/InstructorCalendar/InstructorCalendar.jsx
@@ -20,25 +20,9 @@ function Calendar(){
         <div className="month-holder container bg-light shadow p-4 m-1" >
             <CalNavigation displayReferenceDate={displayReferenceDate} setDisplayReferenceDate={setDisplayReferenceDate}/>
             <CalMonthView todaysDate={todaysDate} displayReferenceDate={displayReferenceDate}/>
-        
-              <Modal.Dialog>
-                <Modal.Header closeButton>
-                  <Modal.Title>Modal title</Modal.Title>
-                </Modal.Header>
-          
-                <Modal.Body>
-                  <p>Modal body text goes here.</p>
-                </Modal.Body>
-          
-                <Modal.Footer>
-                  <Button variant="secondary">Close</Button>
-                  <Button variant="primary">Save changes</Button>
-                </Modal.Footer>
-              </Modal.Dialog>
         </div>
-            
-          
-         
+        
+
     )
 
 }
