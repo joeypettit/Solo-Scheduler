2fde2d078fb4f2037374993d65581b99a7ee7798
Joey Pettit <pettitbass@gmail.com>
2022-10-26 11:47:35 -0500

    tweaked event buttons on reg cal

--- src/components/PortalStudent/LessonRegister/RegistrationCalendar/RegistrationMonthView/RegistrationMonthView.jsx
+++ src/components/PortalStudent/LessonRegister/RegistrationCalendar/RegistrationMonthView/RegistrationMonthView.jsx
@@ -151,7 +151,7 @@ function RegistrationMonthView({displayReferenceDate}){
                           } else if (!thisEvent.registered_students_ids.includes(null) && !thisEvent.registered_students_ids.includes(user.id)){
                             return null;
                           } else if(thisEvent.registered_students_ids.includes(user.id)){
-                            return <div key={index} className='alert alert-success p-1 m-1'><p className='event-text'>{eventText}</p><hr/>
+                            return <div key={index} className='alert alert-success p-1 m-1'><p className='event-text'>{eventText}</p><p className='event-text'>Scheduled!</p><hr/>
                                     <div className='d-flex justify-content-around'>
                                     <button className="btn btn-sm opacity-75 btn-light border shadow-sm" onClick={()=>launchDisplayLessonInfo(thisEvent)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-info-square-fill" viewBox="0 0 16 16">
@@ -174,12 +174,6 @@ function RegistrationMonthView({displayReferenceDate}){
                                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
                                             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                                             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                                          </svg>
-                                        </button>
-                                        <button className="btn btn-sm opacity-75 btn-light border shadow-sm" onClick={()=>launchDeleteConfirmationModal(thisEvent)}>
-                                          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-x-square-fill" viewBox="0 0 16 16">
-                                            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                                           </svg>
                                         </button>
                                       </div>                           
