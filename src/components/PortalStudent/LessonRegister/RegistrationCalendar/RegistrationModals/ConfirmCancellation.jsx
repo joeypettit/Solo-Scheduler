d8eebe296d84890392b378950a484bbf6ebce3ec
Joey Pettit <pettitbass@gmail.com>
2022-10-26 15:06:20 -0500

    delete modal works in student view

--- src/components/PortalStudent/LessonRegister/RegistrationCalendar/RegistrationModals/ConfirmCancellation.jsx
+++ src/components/PortalStudent/LessonRegister/RegistrationCalendar/RegistrationModals/ConfirmCancellation.jsx
@@ -1,18 +1,20 @@
 import {useState, useDispatch} from 'react-redux';
 import {DateTime} from 'luxon';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
-function ConfirmCancellation({lessonToCancel, setCancelModalDisplayed}){
+function ConfirmCancellation({thisLessonInfo, setCancelModalDisplayed,cancelModalDisplayed}){
     const dispatch = useDispatch();
 
-    const startTime = DateTime.fromISO((lessonToCancel.start_time));
-    const endTime = DateTime.fromISO((lessonToCancel.end_time));
+    const startTime = DateTime.fromISO((thisLessonInfo.start_time));
+    const endTime = DateTime.fromISO((thisLessonInfo.end_time));
 
     // this function will remove the student from the selected lesson time
     function CancelReservation(){
         // dispatch id of lesson to be cancelled
         dispatch({
             type: 'REMOVE_LESSON_RESERVATION',
-            payload: lessonToCancel
+            payload: thisLessonInfo
         });
 
         // close this modal
@@ -22,17 +24,29 @@ function ConfirmCancellation({lessonToCancel, setCancelModalDisplayed}){
 
 
     return(
-        <div>
-            <h1>Are you sure you would like to cancel this lesson with {lessonToCancel.first_name}?</h1>
+        <>
+        <Modal show={cancelModalDisplayed}>
+        <Modal.Header>
+          <Modal.Title><h1>Are you sure you would like to cancel this lesson with {thisLessonInfo.first_name}?</h1></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
             <h2>From: {startTime.toLocaleString(DateTime.TIME_SIMPLE)} </h2>
             <h2>To: {endTime.toLocaleString(DateTime.TIME_SIMPLE)}</h2>
             <h2>On: {startTime.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)}</h2>
-            <button onClick={()=> CancelReservation()}>Confirm</button>
-            <button onClick={()=> setCancelModalDisplayed(false)}>Back</button>
-        </div>
-        
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant="danger" onClick={()=>CancelReservation()}>
+                Delete
+            </Button>
+            <Button variant="secondary" onClick={()=>setCancelModalDisplayed(false)}>
+                Close
+            </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
     )
 
 }
 
 export default ConfirmCancellation;
+
