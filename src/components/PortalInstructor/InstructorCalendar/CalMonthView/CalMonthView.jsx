48e19163013da5d4d627975e3fbc639dd28e662e
Joey Pettit <pettitbass@gmail.com>
2022-10-25 19:29:36 -0500

    about to checkout modal

--- src/components/PortalInstructor/InstructorCalendar/CalMonthView/CalMonthView.jsx
+++ src/components/PortalInstructor/InstructorCalendar/CalMonthView/CalMonthView.jsx
@@ -104,9 +104,8 @@ function CalMonthView({displayReferenceDate}){
     // this function will create the jsx for each row of the calendar and wrap each one in a 
     // div with a bootstrap row class. It will then export all of jsx to build out the entire monthly calendar
     function createWeekRows(thisWeeksDates){
-
       return (
-        <div className='row'>
+        <div className='row mx-0'>
         {thisWeeksDates && thisWeeksDates.map((date, index)=>{
           return(
             <div 
@@ -161,12 +160,20 @@ function CalMonthView({displayReferenceDate}){
                                     </div>
                           } else{
                             return <div key={index} className='alert alert-primary p-1 m-1'><p className='event-text'>{eventText}</p><hr/>
-                                    <button className="btn btn-sm opacity-75 btn-light border shadow-sm" onClick={()=>deleteEvent(thisEvent.lesson_id)}>
-                                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-square-fill" viewBox="0 0 16 16">
-                                        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                                      </svg>
-                                    </button>                           
+                                    <div className='d-flex justify-content-around'>
+                                        <button className="btn btn-sm opacity-75 btn-light border shadow-sm" onClick={()=>launchDisplayLessonInfo(thisEvent)}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-info-square-fill" viewBox="0 0 16 16">
+                                              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                                              <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                                            </svg>
+                                          </button>
+                                          <button className="btn btn-sm opacity-75 btn-light border shadow-sm" onClick={()=>deleteEvent(thisEvent.lesson_id)}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-square-fill" viewBox="0 0 16 16">
+                                              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                                              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                            </svg>
+                                          </button>
+                                        </div>                           
                                    </div>
                           }})
                       : null }
@@ -220,7 +227,7 @@ function CalMonthView({displayReferenceDate}){
     useEffect(()=> fetchUserLessons(),[]);
 
     return (
-        <div className="month-holder container-lg m-1 overflow-auto">
+        <div className="container-lg m-1">
           <div className='row'>
             <div className='col'>Monday</div>
             <div className='col'>Tuesday</div>
