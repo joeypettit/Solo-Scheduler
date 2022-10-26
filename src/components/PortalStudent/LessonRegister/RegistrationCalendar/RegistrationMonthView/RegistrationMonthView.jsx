37beefe0f18ec0a56ed8d905c47f9ad3a7f768ad
Joey Pettit <pettitbass@gmail.com>
2022-10-26 15:49:40 -0500

    removed info button from reg calendar

--- src/components/PortalStudent/LessonRegister/RegistrationCalendar/RegistrationMonthView/RegistrationMonthView.jsx
+++ src/components/PortalStudent/LessonRegister/RegistrationCalendar/RegistrationMonthView/RegistrationMonthView.jsx
@@ -20,7 +20,6 @@ function RegistrationMonthView({displayReferenceDate}){
 
     //~~~ Modal Displays
     const [confirmModalDisplayed, setConfirmModalDisplayed] = useState(false);
-    const [successModalDisplayed, setSuccessModalDisplayed] = useState(false);
     const [cancelModalDisplayed, setCancelModalDisplayed] = useState(false);
 
     
@@ -168,12 +167,6 @@ function RegistrationMonthView({displayReferenceDate}){
                           } else if(thisEvent.registered_students_ids.includes(user.id)){
                             return <div key={index} className='alert alert-success p-1 m-1'><p className='event-text'>{eventText}</p><p className='event-text'>Scheduled!</p><hr/>
                                     <div className='d-flex justify-content-around'>
-                                    <button className="btn btn-sm opacity-75 btn-light border shadow-sm" onClick={()=>launchConfirmLessonTimeModal(thisEvent)}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-info-square-fill" viewBox="0 0 16 16">
-                                          <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                                          <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                                        </svg>
-                                      </button>
                                       <button className="btn btn-sm opacity-75 btn-light border shadow-sm" onClick={()=>launchDeleteConfirmationModal(thisEvent)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-x-square-fill" viewBox="0 0 16 16">
                                           <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
@@ -228,7 +221,7 @@ function RegistrationMonthView({displayReferenceDate}){
         {createWeekRows(displayedDates[5])}
 
         {thisLessonInfo && <ConfirmCancellation setCancelModalDisplayed={setCancelModalDisplayed} thisLessonInfo={thisLessonInfo} cancelModalDisplayed={cancelModalDisplayed}/>}
-        {thisLessonInfo && <ConfirmLessonTime setConfirmModalDisplayed={setConfirmModalDisplayed} thisLessonInfo={thisLessonInfo} confirmModalDisplayed={confirmModalDisplayed} setSuccessModalDisplayed={setSuccessModalDisplayed}/>}
+        {thisLessonInfo && <ConfirmLessonTime setConfirmModalDisplayed={setConfirmModalDisplayed} thisLessonInfo={thisLessonInfo} confirmModalDisplayed={confirmModalDisplayed} />}
     </div>
     ) 
 }
